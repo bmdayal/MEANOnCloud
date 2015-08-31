974be73774a2f152fc998c35111a2ccd3c74b891
bmdayal <bmdayal@hotmail.com>
2015-08-30 21:51:48 -0500

    Applied mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('persons', ['Persons']);
+
+app.use(express.static(__dirname));
+
+app.get('/persons', function(req, res){
+	console.log('I received get request');
+
+	db.Persons.find(function(err, data){
+		console.log(data);
+		res.json(data);
+	})
+});
 
 //app.use(express.static(__dirname + "/app/views"));
-app.use(express.static(__dirname));
 app.listen(3000);
 console.log("server running on port 3000");
